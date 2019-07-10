accd39612ec9a03f440d44edb7322cdfca2cde0e
Rodnoy Konstantin <kotrod@gmail.com>
2019-07-10 22:02:14 +0300

    've corrected a little

--- src/lection09_Generics/objects/TwoNum.java
+++ src/lection09_Generics/objects/TwoNum.java
@@ -11,18 +11,18 @@ public class TwoNum <T extends Number, V extends Number> {
     }
     public double toSumUp(){
         BigDecimal result = num1.add(num2);
-        return result.doubleValue();
+        return result.setScale(2,BigDecimal.ROUND_HALF_UP).doubleValue();
     }
     public double toSubtract(){
         BigDecimal result = num1.subtract(num2);
-        return result.doubleValue();
+        return result.setScale(2,BigDecimal.ROUND_HALF_UP).doubleValue();
     }
     public double toMultiply(){
         BigDecimal result = num1.multiply(num2);
-        return result.doubleValue();
+        return result.setScale(2,BigDecimal.ROUND_HALF_UP).doubleValue();
     }
     public double toDivide(){
-        BigDecimal result = num1.divide(num2);
+        BigDecimal result = num1.divide(num2, BigDecimal.ROUND_HALF_UP);
         return result.doubleValue();
     }
 
