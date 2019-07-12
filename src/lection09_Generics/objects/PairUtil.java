a95fa0cc3b18159ca5e3ac91814e8bea1b653df7
Rodnoy Konstantin <kotrod@gmail.com>
2019-07-12 21:55:54 +0300

    PairUtil

--- src/lection09_Generics/objects/PairUtil.java
+++ src/lection09_Generics/objects/PairUtil.java
@@ -2,7 +2,7 @@ package lection09_Generics.objects;
 
 public final class PairUtil {
     public static <T,V> Pair swap(Pair <T,V> ob){
-        Pair pairCopy = new Pair<>(ob.getObj2(),ob.getObj1());
+        Pair <V,T> pairCopy = new Pair<>(ob.getObj2(),ob.getObj1());
         return pairCopy;
     }
 }
