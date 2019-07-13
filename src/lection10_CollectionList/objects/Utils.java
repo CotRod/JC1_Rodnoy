a38976ab918a8fac56f10deaf791cd243b1e355f
Rodnoy Konstantin <kotrod@gmail.com>
2019-07-13 10:53:50 +0300

    10.5

--- src/lection10_CollectionList/objects/Utils.java
+++ src/lection10_CollectionList/objects/Utils.java
@@ -1,27 +1,20 @@
 package lection10_CollectionList.objects;
 
-import java.util.Iterator;
 import java.util.Set;
 import java.lang.Integer;
 import java.util.TreeSet;
 
 public class Utils {
     public static Set intersection(Set<Integer> first, Set<Integer> second) {
-        Iterator<Integer> iter = second.iterator();
-        Set<Integer> newSet = new TreeSet();
-        while (iter.hasNext()) {
-            int i = iter.next();
-            if (first.contains(i)) {
-                newSet.add(i);
-            }
-        }
+
+        Set<Integer> newSet = new TreeSet(first);
+        newSet.retainAll(second);
         System.out.println(newSet);
         return newSet;
     }
 
     public static Set toCombine(Set<Integer> first, Set<Integer> second) {
-        Set<Integer> newSet = new TreeSet<>();
-        newSet.addAll(first);
+        Set<Integer> newSet = new TreeSet(first);
         newSet.addAll(second);
         System.out.println(newSet);
         return newSet;
