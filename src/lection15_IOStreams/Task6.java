d38d5f535b06890987e288f16d78b1fdf579beed
Rodnoy Konstantin <kotrod@gmail.com>
2019-07-22 19:53:34 +0300

    lection 15_6

--- src/lection15_IOStreams/Task6.java
+++ src/lection15_IOStreams/Task6.java
@@ -2,6 +2,8 @@ package lection15_IOStreams;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Task6 {
     public static void main(String[] args) {
@@ -11,14 +13,19 @@ public class Task6 {
     }
 
     private static void showList(File file) {
+        Pattern pattern = Pattern.compile("\\\\");
+        Matcher matcher = pattern.matcher(file.getPath());
+        while (matcher.find()){
+            System.out.print("|\t");
+        }
+
         if (file.isDirectory()) {
             System.out.println(file.getName() + " is a Folder, that contains :");
             for (File f : file.listFiles()) {
-                System.out.print("\t");
                 showList(f);
             }
         } else if (file.isFile()) {
-            System.out.println("\t" + file.getName() + " is a File.");
+            System.out.println(file.getName() + " is a File.");
         }
     }
 }
