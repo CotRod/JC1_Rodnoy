3c99bf0e62686fa2f1568007ccd168e612f5161d
Rodnoy Konstantin <kotrod@gmail.com>
2019-07-23 08:01:27 +0300

    lection 14

--- src/lection14_Regexp/Task5.java
+++ src/lection14_Regexp/Task5.java
@@ -8,7 +8,7 @@ public class Task5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s;
-        String p = "(?<!.)(((1\\d\\d)|(2[0-4]\\d)|(25[0-5])|([1-9]\\d)|(\\d))\\.){3}((1\\d\\d)|(2[0-4]\\d)|(25[0-5])|([1-9]\\d)|(\\d))(?!.)";
+        String p = "^(((1\\d\\d)|(2[0-4]\\d)|(25[0-5])|([1-9]\\d)|(\\d))\\.){3}((1\\d\\d)|(2[0-4]\\d)|(25[0-5])|([1-9]\\d)|(\\d))$";
         boolean valid;
         do {
             System.out.println("Input IPv4: ");
