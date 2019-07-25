58bdc26139c0f8491a85f750173ff5319a1adc56
Rodnoy Konstantin <kotrod@gmail.com>
2019-07-25 22:17:24 +0300

    lection 15.3

--- src/lection15_IOStreams/Task3.java
+++ src/lection15_IOStreams/Task3.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 public class Task3 {
     public static void main(String[] args) {
         String path = "src" + File.separator + "lection15_IOStreams" + File.separator + "Text.txt";
-        Pattern p = Pattern.compile("\\b\\w*(\\w)\\b\\s?\\n?\\b\\1\\w*\\b");
+        Pattern p = Pattern.compile("\\b\\w*(\\w)\\b\\s?\\n?\\b\\1\\w*\\b",Pattern.CASE_INSENSITIVE);
         try (FileInputStream fis = new FileInputStream(path);
              InputStreamReader isr = new InputStreamReader(fis);
              BufferedReader br = new BufferedReader(isr)) {
