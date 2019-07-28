1ef64cf61084beb52a52db55ec44102075ec001b
Rodnoy Konstantin <kotrod@gmail.com>
2019-07-28 12:45:15 +0300

    Date API

--- src/lection30_DateApi/Task8.java
+++ src/lection30_DateApi/Task8.java
@@ -10,14 +10,16 @@ import static java.time.temporal.ChronoField.DAY_OF_YEAR;
 public class Task8 {
     public static void main(String[] args) {
         LocalDate today = LocalDate.now();
-        LocalDate newYear = today.with(TemporalAdjusters.firstDayOfNextYear());
         LocalDate newYear2 = today.with(NEXT_YEAR);
 
-        System.out.println(newYear.getDayOfWeek() + " " + newYear);
+        System.out.println(today.getDayOfWeek() + " " + today);
         System.out.println(newYear2.getDayOfWeek() + " " + newYear2);
     }
 
-    private static TemporalAdjuster NEXT_YEAR = TemporalAdjusters.ofDateAdjuster(localDate ->
-            localDate.with(DAY_OF_YEAR, 1).plusYears(1)
+    private static TemporalAdjuster NEXT_YEAR = TemporalAdjusters.ofDateAdjuster(localDate -> {
+        if(localDate.getDayOfYear()>182) {
+            return localDate.with(DAY_OF_YEAR, 1).plusYears(1);
+        }else return localDate.with(DAY_OF_YEAR,1);
+    }
     );
 }
